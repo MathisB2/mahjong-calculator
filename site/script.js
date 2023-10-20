a8526ca9d397f62e346d55b9a3bb17f774c6a89a
NAMANI SAMUEL p2204906 <samuel.namani@etu.univ-lyon1.fr>
2023-10-20 10:12:46 +0000

    Update 19 files
    - /server/.idea/.gitignore
    - /server/.idea/misc.xml
    - /server/.idea/modules.xml
    - /server/src/WebSocketServerExample.java
    - /server/.gitignore
    - /server/server.iml
    - /mavenServer/.idea/.gitignore
    - /mavenServer/.idea/encodings.xml
    - /mavenServer/.idea/misc.xml
    - /mavenServer/src/main/java/MainClass.java
    - /mavenServer/src/main/java/WebSocketServerExample.java
    - /mavenServer/.gitignore
    - /mavenServer/pom.xml
    - /client/index.html
    - /site/calcul.html
    - /site/index.html
    - /site/script.js
    - /site/style.css
    - /site/img/fond.avif

---
+++ site/script.js
@@ -0,0 +1,25 @@
+const textBox=document.getElementById("message")
+const sendButton=document.getElementById("send")
+
+var connection = new WebSocket('ws://localhost:4444');
+
+
+connection.onopen = function () {
+    console.log('Connected!');
+    connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+    console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+    console.log('Server: ' + e.data);
+};
+
+
+sendButton.onclick=function (){
+    connection.send(textBox.value);
+}
