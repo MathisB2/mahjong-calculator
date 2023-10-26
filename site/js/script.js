3e2bcc2961582775c3d0f5a5e483af3d6fa17cbd
NAMANI SAMUEL p2204906 <samuel.namani@etu.univ-lyon1.fr>
2023-10-26 09:15:21 +0000

    Update 2 files
    - /site/index.html
    - /site/js/script.js

--- site/js/script.js
+++ site/js/script.js
@@ -1,3 +1,55 @@
+// envoie img 
+
+
+
+function convertDataURIToBinary(dataURI) {
+	var BASE64_MARKER = ';base64,';
+	var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+	var base64 = dataURI.substring(base64Index);
+	var raw = window.atob(base64);
+	var rawLength = raw.length;
+	var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+	for(i = 0; i < rawLength; i++) {
+		array[i] = raw.charCodeAt(i);
+	}
+	return array;
+}
+
+
+
+function readFile(evt) {
+    var f = evt.target.files[0]; 
+
+    if (f) {
+		if ( /(jpe?g|png|gif)$/i.test(f.type) ) {
+			var r = new FileReader();
+			r.onload = function(e) { 
+				var base64Img = e.target.result;
+                connection.send(base64Img);
+			}
+			r.readAsDataURL(f);
+		} else { 
+			alert("Failed file type");
+		}
+    } else { 
+		alert("Failed to load file");
+    }
+}
+
+
+document.getElementById('file').addEventListener('change', readFile, false);
+
+
+
+
+
+// envoie message
+
+
+
+
+
 const textBox=document.getElementById("message")
 const sendButton=document.getElementById("send")
 
@@ -27,9 +79,11 @@ sendButton.onclick=function (){
 
 
 
-
-
 // site web
+
+
+
+
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -38,3 +92,10 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+
+
+
+
+
