60875f016f48f85f25c666397b82e9c705a2c8b2
MathisB2 <mathis.bodin2@gmail.com>
2024-01-06 18:57:55 +0100

    dynamic generation of footer from js

--- client/js/main.js
+++ client/js/main.js
@@ -4,6 +4,7 @@ import {startSettings} from "./HandController/gameSettings.js";
 import {startHeader} from "./GlobalHtmlObjects/Header/header.js";
 import {startAbout} from "./GlobalHtmlObjects/aboutInfos/about.js";
 import {startAnimations} from "./GlobalHtmlObjects/animations/animations.js";
+import {startFooter} from "./GlobalHtmlObjects/footer/footer.js";
 
 function main(){
     startHeader().then(
@@ -13,5 +14,6 @@ function main(){
     startSettings().then();
     startAbout().then();
     startAnimations().then();
+    startFooter().then();
 }
 main();
