99ed8a522c3186e7ab34a24fea5cc4cabef7440d
MathisB2 <mathis.bodin2@gmail.com>
2024-02-21 17:34:03 +0100

    cluster doc

--- server/src/Clustering/Cluster.java
+++ server/src/Clustering/Cluster.java
@@ -1,16 +1,27 @@
 package Clustering;
 
 import org.json.JSONArray;
-
 import java.util.ArrayList;
 
+/**
+ * Class to manage ClusterPoint Objects. Usage is similar to ArrayList<ClusterPoint>
+ */
 public class Cluster extends ArrayList<ClusterPoint> {
+
+    /**
+     * Function to merge a cluster to the current one
+     * @param cluster the cluster to merge
+     */
     public void concat(Cluster cluster){
         for(var point : cluster){
             this.add(point);
         }
     }
 
+    /**
+     * Function to convert the cluster to JSON
+     * @return the JSON object of the cluster
+     */
     public JSONArray toJSONObject() {
         JSONArray obj = new JSONArray();
 
