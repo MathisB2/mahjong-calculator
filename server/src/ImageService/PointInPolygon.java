307e5dc92222888a0dc794129d49a76b4439db18
maxence caron <96260574+maaxtislemars@users.noreply.github.com>
2024-02-21 16:40:53 +0100

    doc

--- server/src/ImageService/PointInPolygon.java
+++ server/src/ImageService/PointInPolygon.java
@@ -2,7 +2,17 @@ package ImageService;
 
 import org.opencv.core.Point;
 
+/**
+ * class which test if a point is in a polygon
+ */
 public class PointInPolygon {
+    /**
+     * Constructor of the class
+     * test if a point is in a polygon
+     * @param polygon Polygon Point table
+     * @param point Point to test
+     * @return a boolean which expresses the result
+     */
     public static boolean pointInPolygon(Point[] polygon, Point point) {
         boolean odd = false;
         for (int i = 0, j = polygon.length - 1; i < polygon.length; i++) {
